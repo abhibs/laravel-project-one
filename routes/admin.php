a9f82bf8f8f6ae3c41b4f3d958cc0440b260aaf8
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-11 01:24:14 +0530

    admin login page design completed

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/test', function () {
     echo "Abhiram";
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/login', [AdminController::class, 'login'])->name('admin-login');
 });
