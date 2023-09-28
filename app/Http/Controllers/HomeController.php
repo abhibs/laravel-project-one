74d06862b2b7074d7140c706fa7458c005e4b583
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-28 17:56:51 +0530

    faq completed in user site

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\About;
 use App\Models\Client;
+use App\Models\Faq;
 use App\Models\Profile;
 use App\Models\Service;
 use App\Models\Team;
@@ -18,7 +19,9 @@ class HomeController extends Controller
         $aboutdata = About::find(1);
         $services = Service::where('status', 1)->take(4)->latest()->get();
         $teamdatas = Team::where('status', 1)->get();
+        $faqdatas = Faq::where('status', 1)->latest()->get();
 
-        return view('welcome', compact('profiledata', 'clients', 'aboutdata', 'services', 'teamdatas'));
+
+        return view('welcome', compact('profiledata', 'clients', 'aboutdata', 'services', 'teamdatas', 'faqdatas'));
     }
 }
